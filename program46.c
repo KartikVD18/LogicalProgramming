1e38a4d0f21481f74b3c3dc7e30cc4a43544ae60
Kartik Dhole <kartikdhole007@gmail.com>
2025-01-24 20:10:07 +0530

    Check Divisiblity

---
+++ program46.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckDivisible(int iNo)
+{
+    if(((iNo%3)==0)&&((iNo%5)==0))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = 0;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+
+    bRet = CheckDivisible(iValue);
+
+    if(bRet == true)
+    {
+        printf("%d is divisible by 3 & 5\n",iValue);
+    }
+    else
+    {
+        printf("%d is not divisible by 3 & 5\n",iValue);
+    }
+
+    return 0;
+}
+
+/*
+-------------------------------------------------------
+    Operand 1       Operand 2       &&          ||
+-------------------------------------------------------
+    true            true            true        true
+    true            false           false       true
+    false           true            false       true
+    false           false           false       false
+-------------------------------------------------------
+*/
